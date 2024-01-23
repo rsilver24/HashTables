b847a1a69732a081256c1170673f3d08d027bb76
rsilver24 <rsilver24@kentdenver.org>
2024-01-23 08:48:55 -0700

    more organization

--- HashTables/src/HashTables.java
+++ HashTables/src/HashTables.java
@@ -1,4 +1,27 @@
 import java.util.HashMap;
 public class HashTables {
-    
+
+    // Instance Data
+
+    private int HashTableLength;
+
+    // Constructors
+
+    public HashTables(int capacity){
+
+    }
+
+    // Methods
+
+    public boolean put(String key, String value){
+
+    }
+
+    public String get(String key){
+
+    }
+
+    private int hashCode(String key){
+
+    }
 }
